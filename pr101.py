3a017ddda147585266edc0afd6382640251ab31b
Evan C Turner <evan.turner@gmail.com>
2013-02-02 18:24:43 -0500

    refactored, switched to data point (x, f(x)) format

--- pr101.py
+++ pr101.py
@@ -4,41 +4,34 @@
 def u(n, m): return sum([(-1)**i * n**i for i in xrange(m+1)])
 
 # takes n + 1 data points -- (x, f(x)) -- and generates divided differences
-def get_newton_diffs(x, fx):
-    n = len(x) - 1
+def get_newton_diffs(data_points):
+    n = len(data_points) - 1    # degree of polynomial
 
-    diffs = []
+    diffs = [[dp[1] for dp in data_points]]         # diff level 0
     
-    diff_list = fx              # diff level 0
-    diffs.append(diff_list)     # add this set of divided differences
-    diff_list_prev = diff_list
+    for diff_level in xrange(1, n+1):
     
-    diff_level = 1
-    
-    while (diff_level <= n):
-        diff_list = []
+        diffs.append([])
         i, j = 1, 0
+
         while i <= n - (diff_level-1):
-            diff_list.append( 
-                    (diff_list_prev[i] - diff_list_prev[j]) / (x[i + (diff_level-1)] - x[j])
+            diffs[diff_level].append(
+                    (diffs[diff_level-1][i] - diffs[diff_level-1][j]) / 
+                    (data_points[(diff_level-1) + i][0] - data_points[j][0])
                 )
             i += 1
-            j += 1            
+            j += 1     
         
-        diffs.append(diff_list)     # add this set of divided differences
-        diff_list_prev = diff_list
-        diff_level += 1
-
     return diffs
 
-def eval_newton_poly(diffs, x_vals, eval=0):
-    n = len(x_vals) - 1  # n + 1 data points for polynomial order n
+def eval_newton_poly(diffs, data_points, eval=0):
+    n = len(data_points) - 1  # n + 1 data points for polynomial order n
     fx_eval = 0
     
     for i in xrange(0, n+1):
         coeff = 1
         for j in xrange(i):
-            coeff *= (eval - x_vals[j])
+            coeff *= (eval - data_points[j][0])
         fx_eval += coeff * diffs[i][0]
     
     return fx_eval
@@ -55,19 +48,22 @@ def generating_fncn(x, fncn=0):
 
 def solve_pr101(k_max, fncn=0):
     epsilon = 1e-03
+    max_n = 20
     total_FIT = 0
     
     for i in xrange(1, k_max+1):
-        k = xrange(1, i+1)
-        f_k = [generating_fncn(x, fncn) for x in k]
-        diffs = get_newton_diffs(k, f_k)
+    
+        data_points = [(x, generating_fncn(x, fncn)) for x in xrange(1, i+1)]
         
-        for x in xrange(len(k), len(k) + 20):
-            eval_newton_x = eval_newton_poly(diffs, k, x)   #OP(k,n)
-            gf_x = generating_fncn(x, fncn)
-            if abs(eval_newton_x  - gf_x) > epsilon:
-                total_FIT += eval_newton_x
+        diffs = get_newton_diffs(data_points)
+        n = len(data_points)
+        
+        for x in xrange(n, max_n):
+            OP_k_n = eval_newton_poly(diffs, data_points, x)
+            f_x = generating_fncn(x, fncn)
+            
+            if abs(OP_k_n  - f_x) > epsilon:
+                total_FIT += OP_k_n   # This is the First Incorrect Term
                 break
-
 
     return total_FIT
